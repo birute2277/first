cc34327c7180d0af2ec10a91e4c3f4667b0de259
Birute Petrauskienė <petrauskiene.birute@gmail.com>
2021-11-29 11:36:28 +0200

    nnn

--- git1.js
+++ git1.js
@@ -1,1 +1,16 @@
-console.log("Pirmasis")
+console.log("Pirmasis git darbas")
+const button=document.querySelector("button")
+const box= document.querySelector(".box")
+
+let trigger=true
+
+button.onclick=()=> {
+    trigger = !trigger
+    if (trigger) {
+        button.innerText = "Turn On"
+        box.style.display = "block"
+    } else {
+        button.innerText = "Turn Off"
+        box.style.display = "none"
+    }
+}
