e1c82d03021698328c3a1a26ca4db3a0e2a781ab
Birute Petrauskienė <petrauskiene.birute@gmail.com>
2021-11-29 12:09:36 +0200

    Random

--- git1.js
+++ git1.js
@@ -1,16 +1,34 @@
 console.log("Pirmasis git darbas")
-const button=document.querySelector("button")
-const box= document.querySelector(".box")
 
-let trigger=true
+const button=document.querySelector("button")
+const box=document.querySelector(".box")
+
+
+document.getElementsByClassName("box").innerHTML=Math.random()
+
+
+let x = 0
+
+//document.getElementById("demo").innerHTML = x;
 
 button.onclick=()=> {
-    trigger = !trigger
-    if (trigger) {
-        button.innerText = "Turn On"
-        box.style.display = "block"
-    } else {
-        button.innerText = "Turn Off"
-        box.style.display = "none"
-    }
+    x = Math.floor((Math.random() * 10) + 1)
+   console.log("click adnaka", x)
+
 }
+
+
+
+// ++++++++++++++++++++++++++++++++++++
+// let trigger=true
+//
+// button.onclick=()=> {
+//     trigger = !trigger
+//     if (trigger) {
+//         button.innerText = "Turn On"
+//         box.style.display = "block"
+//     } else {
+//         button.innerText = "Turn Off"
+//         box.style.display = "none"
+//     }
+// }
